852a8606ec2dd315eba862a31f9dd65f746a97ab
Jean-Christio Martin <admin@marjc.ch>
2025-04-04 20:13:48 +0200

    chore: bump version to 1.3.2 in package.json and update CDN links in documentation

--- src/AFS.js
+++ src/AFS.js
@@ -19,7 +19,7 @@ import { DateFilter } from "./features/DateFilter";
 import { debounce } from "./utils";
 
 // Version
-export const VERSION = '1.3.1';
+export const VERSION = '1.3.2';
 
 export class AFS extends EventEmitter {
   /**
